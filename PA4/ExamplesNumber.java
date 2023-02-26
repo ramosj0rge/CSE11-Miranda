ca2afcdd831ab9ec8c51ed25aa197df6652c4b63
ramosj0rge <ramosj8812@gmail.com>
2023-02-25 23:04:14 -0800

    change

--- PA4/ExamplesNumber.java
+++ PA4/ExamplesNumber.java
@@ -66,14 +66,83 @@ class Fraction implements Number{
         return String.valueOf(n) + "/" + String.valueOf(d);
     }
     public double toDouble(){
-        double temp = this.n / this.d;
+        double a = this.n;
+        double b = this.d;
+        double temp = a / b;
         return temp;
     }
 }
 
-//Exploration 
+//Testing
 
 class ExamplesNumber{
-    Fraction arithmetic1 = new Fraction(1, 10);
-    double fraction1 = arithmetic1.toDouble();
+    WholeNumber num1 = new WholeNumber(8);
+    WholeNumber num2 = new WholeNumber(6);
+    int test1 = num1.numerator();
+    int test2 = num1.denominator();
+    int test19 = num2.numerator();
+    int test20 = num2.denominator();
+    Number test3 = num1.add(num2);
+    Number test4 = num1.multiply(num2);
+    String test5 = num1.toString();
+    String test6 = num2.toString();
+    double test7 = num1.toDouble();
+    double test8 = num2.toDouble();
+
+    Fraction num3 = new Fraction(2, 3);
+    Fraction num4 = new Fraction(3, 4);
+    int test9 = num3.numerator();
+    int test10 = num3.denominator();
+    int test11 = num4.numerator();
+    int test12 = num4.denominator();
+    Number test13 = num3.add(num4);
+    Number test14 = num3.multiply(num4);
+    String test15 = num3.toString();
+    String test16 = num4.toString();
+    double test17 = num3.toDouble();
+    double test18 = num4.toDouble();
+
+    //Exploration
+    double ex1 = 0.1 + 0.2 + 0.3;
+    double ex2 = 0.1 + (0.2 + 0.3);
+
+    Fraction ex3 = new Fraction(1, 10);
+    Fraction ex4 = new Fraction(2, 10);
+    Fraction ex5 = new Fraction(3, 10);
+
+    Number test21 = ex3.add(ex4);
+    Number test22 = ex5.add(test21);
+    String test23 = test22.toString();
+
+    Number test24 = ex5.add(ex4);
+    Number test25 = ex3.add(test24);
+    String test26 = test25.toString();
+
+    void testTweets(Tester t){
+        t.checkExpect(this.test1, 8);
+        t.checkExpect(this.test2, 1);
+        t.checkExpect(this.test19, 6);
+        t.checkExpect(this.test20, 1);
+        t.checkExpect(this.test3, new Fraction(14,1));
+        t.checkExpect(this.test4, new Fraction(48, 1));
+        t.checkExpect(this.test5, "8");
+        t.checkExpect(this.test6, "6");
+        t.checkExpect(this.test7, 8.0);
+        t.checkExpect(this.test8, 6.0);
+
+        t.checkExpect(this.test9, 2);
+        t.checkExpect(this.test10, 3);
+        t.checkExpect(this.test11, 3);
+        t.checkExpect(this.test12, 4);
+        t.checkExpect(this.test13, new Fraction(17,12));
+        t.checkExpect(this.test14, new Fraction(6, 12));
+        t.checkExpect(this.test15, "2/3");
+        t.checkExpect(this.test16, "3/4");
+        double a = 2;
+        double b = 3;
+        double c = 4;
+        t.checkExpect(this.test17, a/b);
+        t.checkExpect(this.test18, b/c);
+    }
+
 }
--- PA4/Tweets.java
+++ PA4/Tweets.java
@@ -49,7 +49,7 @@ class ReplyTweet implements Tweet{
         return likes + replyTo.totalLikes();
     }
     public String unrollThread(){
-        return replyTo.unrollThread() + "\n" + author + "\n" + likes +" likes" + "\n" + contents + "\n";
+        return replyTo.unrollThread() + author + "\n" + likes +" likes" + "\n" + contents + "\n";
     }
 }
 
@@ -67,11 +67,26 @@ class Tweets{
     
     TextTweet testB = new TextTweet("These must come out as false", 2, "anonymoususer");
     boolean test7 = this.testB.isStartOfThreadBy("adidas");
-    int test8 = this.testA.totalLikes();
+    int test8 = this.testB.totalLikes();
     String test9 = this.testB.unrollThread();
     ReplyTweet testB2 = new ReplyTweet("So, tell me, does it come out as false?", 23, "notanonymous", testB);
     boolean test10 = this.testB2.isStartOfThreadBy("anonymoususer");
-    int test11 = this.testB.totalLikes();
-    String test12 = this.testB.unrollThread();
+    int test11 = this.testB2.totalLikes();
+    String test12 = this.testB2.unrollThread();
 
+    void testTweets(Tester t){
+        t.checkExpect(this.test1, true);
+        t.checkExpect(this.test2, 5);
+        t.checkExpect(this.test3,"sosajorge" + "\n" + 5 + " likes" + "\n" + "Pretend this is a real tweet" + "\n");
+        t.checkExpect(this.test4, true);
+        t.checkExpect(this.test5, 218);
+        t.checkExpect(this.test6, "sosajorge" + "\n" + 5 + " likes" + "\n" + "Pretend this is a real tweet" + "\n"  + "sosajorge" + "\n" + 213 + " likes" + "\n" + "But it isn't a real tweet" + "\n");
+
+        t.checkExpect(this.test7, false);
+        t.checkExpect(this.test8, 2);
+        t.checkExpect(this.test9,"anonymoususer" + "\n" + 2 + " likes" + "\n" + "These must come out as false" + "\n");
+        t.checkExpect(this.test10, false);
+        t.checkExpect(this.test11, 25);
+        t.checkExpect(this.test12, "anonymoususer" + "\n" + 2 + " likes" + "\n" + "These must come out as false"  + "\n" + "notanonymous" + "\n" + 23 + " likes" + "\n" + "So, tell me, does it come out as false?" + "\n");
+    }
 }
